ca8468ea5ae362e74c256a5fbaf68a78cecb7a75
joemacKE <josephmuga2020@gmail.com>
2025-08-10 14:47:45 +0300

    Added restriction for one user to only post one rating/review per movie.

--- MOVIES/watchlist_app/api/views.py
+++ MOVIES/watchlist_app/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 from rest_framework import status
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 # from rest_framework import mixins
 
 
@@ -19,10 +20,18 @@ class ReviewList(generics.ListAPIView):
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=watchlist)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie")
+
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
